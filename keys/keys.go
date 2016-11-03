bcc51e7bf531768a417d29ad2d67c3a1a610efca
konojunya <junya03dance@gmail.com>
2016-11-04 04:24:57 +0900

    keyを少し変更

--- keys/keys.go
+++ keys/keys.go
@@ -8,5 +8,6 @@ import (
 func GetTwitterApi() *anaconda.TwitterApi {
 	anaconda.SetConsumerKey(Getenv("TWITTER_CONSUMER_KEY"))
 	anaconda.SetConsumerSecret(Getenv("TWITTER_CONSUMER_SECRET"))
-	return anaconda.NewTwitterApi(Getenv("TWITTER_ACCESS_TOKEN"), Getenv("TWITTER_ACCESS_TOKEN_SECRET"))
+	api := anaconda.NewTwitterApi(Getenv("TWITTER_ACCESS_TOKEN"), Getenv("TWITTER_ACCESS_TOKEN_SECRET"))
+	return api
 }
